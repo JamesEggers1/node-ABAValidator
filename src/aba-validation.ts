67e64b6bab17bf8720d2e91e06a150ac5c4e21e7
James Eggers <james.r.eggers@gmail.com>
2020-08-17 19:52:53 -0500

    Updated syntax and added eslint config

--- src/aba-validation.ts
+++ src/aba-validation.ts
@@ -1,24 +1,19 @@
 "use strict";
-module.exports = (function(){
-	function validate(routingNumber: string){
-		var match = routingNumber.match(/^\s*([\d]{9})\s*$/);
+module.exports = {
+	validate: (routingNumber: string) => {
+		const match = routingNumber.match(/^\s*([\d]{9})\s*$/);
 		if (!match) {
 			return false;
 		}
 
-		var weights = [3, 7 ,1];
-		var aba = match[1];
+		const weights = [3, 7 ,1];
+		const aba = match[1];
 
-		var sum = 0;
-		for (var i=0 ; i<9 ; ++i) {
-			// using charAt for IE7 support
+		let sum = 0;
+		for (let i=0 ; i<9 ; ++i) {
 			sum += +aba.charAt(i) * weights[i % 3];
 		}
 
 		return (sum !== 0 && sum % 10 === 0);
 	}
-	
-	return {
-		validate: validate
-	};
-} ());
+};
